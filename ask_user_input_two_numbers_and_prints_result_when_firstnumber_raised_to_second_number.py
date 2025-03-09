2d3b7a90a261ee49ea6b8b8cb78b764c22d2d992
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:31:51 +0800

    FinishedCode: user input, exponent equation, prints result

--- ask_user_input_two_numbers_and_prints_result_when_firstnumber_raised_to_second_number.py
+++ ask_user_input_two_numbers_and_prints_result_when_firstnumber_raised_to_second_number.py
@@ -1,5 +1,12 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
-#ask user input two numbers
+#ask user input two numbers\
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 #making an equation for exponent
+exponent = num1**num2
+
 #prints the result of first number raised to second number
+print(f"The result is: {exponent}")
+
