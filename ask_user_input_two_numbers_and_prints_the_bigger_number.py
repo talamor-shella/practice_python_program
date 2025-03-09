c43e024dde757357ed15afe9046a6210ed171251
talamor-shella <shellatalamor@gmail.com>
2025-03-09 22:32:52 +0800

    AskUserInput

--- ask_user_input_two_numbers_and_prints_the_bigger_number.py
+++ ask_user_input_two_numbers_and_prints_the_bigger_number.py
@@ -1,6 +1,9 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 #Ask user to input two numbers
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter a number: "))
+
 #Make a variable for determining the bigger number
 #prints the bigger number
 
