1757843c7e26fc8cfbae500b24748005a0df0404
talamor-shella <shellatalamor@gmail.com>
2025-03-09 22:28:41 +0800

    AddedPseudocode

---
+++ ask_user_input_two_numbers_and_prints_the_bigger_number.py
@@ -0,0 +1,6 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+#Ask user to input two numbers
+#Make a variable for determining the bigger number
+#prints the bigger number
+
