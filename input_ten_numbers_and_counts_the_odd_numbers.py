8e57f3a9b1f7ee6659e1a41cd2d0cdc940f645be
talamor-shella <shellatalamor@gmail.com>
2025-03-10 00:13:14 +0800

    AddedPseudocode

--- input_ten_numbers_and_counts_the_odd_numbers.py
+++ input_ten_numbers_and_counts_the_odd_numbers.py
@@ -1,2 +1,5 @@
 #Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
+#for loop to input ten numbers 
+#if statement to check if the number is odd 
+#counts the numbers of odd using addition assignment operato
