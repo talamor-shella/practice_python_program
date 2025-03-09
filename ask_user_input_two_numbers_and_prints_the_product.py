1a5322715b51ba51d34f127ccacd1610996d98be
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:15:18 +0800

    FinishedCodeThatPrintsTheProduct

--- ask_user_input_two_numbers_and_prints_the_product.py
+++ ask_user_input_two_numbers_and_prints_the_product.py
@@ -1,5 +1,11 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers
 
 #ask user to input two numbers 
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
+
 #variable for the product of two numbers
+product = num_1 * num_2
+
 #prints the product 
+print(f"The product is: {product}")
