b793ac0727fc6889e29c67aad8a82be7df05ada3
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:08:54 +0800

    AddedPseudocode

--- ask_user_input_two_numbers_and_prints_the_product.py
+++ ask_user_input_two_numbers_and_prints_the_product.py
@@ -1,1 +1,5 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers
+
+#ask user to input two numbers 
+#variable for the product of two numbers
+#prints the product 
