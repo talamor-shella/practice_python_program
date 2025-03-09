ffa0c0549c5816c162459da100b090d0b23562c4
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:29:24 +0800

    AddedPseudocode

--- ask_user_input_two_numbers_and_prints_result_when_firstnumber_raised_to_second_number.py
+++ ask_user_input_two_numbers_and_prints_result_when_firstnumber_raised_to_second_number.py
@@ -1,1 +1,5 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+#ask user input two numbers
+#making an equation for exponent
+#prints the result of first number raised to second number
