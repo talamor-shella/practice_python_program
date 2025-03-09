a8ee85927a74767c80508c728096031b64f3605c
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:02:32 +0800

    AddedPseudoCode

--- ask_user_input_two_numbers_and_prints_sum_of_numbers.py
+++ ask_user_input_two_numbers_and_prints_sum_of_numbers.py
@@ -1,1 +1,5 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+#ask user to input two numbers
+#variable for the sum of two numbers
+#prints the sum 
