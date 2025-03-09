1c22361a550db6069bce4b1a1b8f2a91839dac5a
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:43:13 +0800

    FinishedCode: using list, for loop, variable for the sum of all numbers, prints the sum

--- input_ten_numbers_and_sum_of_all_numbers.py
+++ input_ten_numbers_and_sum_of_all_numbers.py
@@ -1,5 +1,12 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers
 
 #Make an empty list then use for loop to input ten numbers and append it to empty list
-#prints the sum of ten numbers
+numbers = []
 
+for i in range(10):
+    num = int(input(f"Enter a number {i+1}: "))
+    numbers.append(num)
+
+#prints the sum of ten numbers
+sum_of_numbers = sum(numbers)
+print(sum_of_numbers)
