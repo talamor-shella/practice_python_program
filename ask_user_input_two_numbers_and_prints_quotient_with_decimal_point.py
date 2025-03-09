6470451d86d46b5827f719b0409e3fba57683ff4
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:24:53 +0800

    Pseudocode,try and except and prints the quotient

--- ask_user_input_two_numbers_and_prints_quotient_with_decimal_point.py
+++ ask_user_input_two_numbers_and_prints_quotient_with_decimal_point.py
@@ -1,2 +1,10 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+#Try and Except function, ask user to input two numbers, prints the quotient
+try: 
+    num1 = float(input("Enter a number: "))
+    num2 = float(input("Enter a number: "))
+    quotient = num1 / num2
+    print(f"The quotient is: {quotient}")
 
+except ZeroDivisionError:
+    print("Can't divide a number by zero")
