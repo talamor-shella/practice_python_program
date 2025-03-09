b8c3420cdfbc2a806873a53f3264858741d9fc38
talamor-shella <shellatalamor@gmail.com>
2025-03-09 22:54:35 +0800

    AskUserToInputNumbers

--- ask_user_input_two_numbers_and_prints_equal_when_same_numbers.py
+++ ask_user_input_two_numbers_and_prints_equal_when_same_numbers.py
@@ -1,1 +1,9 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+#ask user to input two numbers
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
+
+#if statement to check if two numbers are equal
+#prints equal when numbers are the same
+
