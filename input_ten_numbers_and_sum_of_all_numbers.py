726996661064d8433ada5cc6570f305d6c1c7686
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:36:42 +0800

    AddedPseudocode

--- input_ten_numbers_and_sum_of_all_numbers.py
+++ input_ten_numbers_and_sum_of_all_numbers.py
@@ -1,2 +1,5 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers
 
+#Make an empty list then use for loop to input ten numbers and append it to empty list
+#prints the sum of ten numbers
+
