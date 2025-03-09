323da1481561625a0aa13404994ea7c13d852a8a
talamor-shella <shellatalamor@gmail.com>
2025-03-09 23:05:58 +0800

    FinishedCodeThatPrintsTheSum

--- ask_user_input_two_numbers_and_prints_sum_of_numbers.py
+++ ask_user_input_two_numbers_and_prints_sum_of_numbers.py
@@ -1,5 +1,11 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 #ask user to input two numbers
+num_1 = int(input("Enter a number: "))
+num_2 = int(input("Enter a number: "))
+
 #variable for the sum of two numbers
+sum_1 = num_1 + num_2
+
 #prints the sum 
+print(f"The sum is: {sum_1}")
