ac2ef85e8fd78c9b2ae8c16b82b20b9b5ac14482
talamor-shella <shellatalamor@gmail.com>
2025-03-10 00:18:08 +0800

    FinishedCode: for loop, if statement, prints the number of odd

--- input_ten_numbers_and_counts_the_odd_numbers.py
+++ input_ten_numbers_and_counts_the_odd_numbers.py
@@ -1,5 +1,10 @@
 #Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 #for loop to input ten numbers 
-#if statement to check if the number is odd 
-#counts the numbers of odd using addition assignment operato
+odd_count = 0 
+for i in range(10):
+    user_input = int(input(f"Enter a number {i+1:}: "))
+    if user_input % 2 != 0: #if statement to check if the number is odd 
+        odd_count += 1 #counts the numbers of odd 
+
+print (f"There are {odd_count} odd numbers.") 
