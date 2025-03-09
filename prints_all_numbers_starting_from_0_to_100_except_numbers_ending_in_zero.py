82254ecafb531ed682a40c7780a945ba3e4a6f11
talamor-shella <shellatalamor@gmail.com>
2025-03-10 00:30:03 +0800

    AddedPseudocode

--- prints_all_numbers_starting_from_0_to_100_except_numbers_ending_in_zero.py
+++ prints_all_numbers_starting_from_0_to_100_except_numbers_ending_in_zero.py
@@ -1,1 +1,5 @@
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero
+
+#for loop from 0 to 100
+#if statement to exclude numbers ending in zero
+#prints all the numbers except numbers with zero
