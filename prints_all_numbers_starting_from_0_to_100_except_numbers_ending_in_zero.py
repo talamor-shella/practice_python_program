9f8062276d1c0559caa9316d54c73f3226bdb7ec
talamor-shella <shellatalamor@gmail.com>
2025-03-10 00:34:43 +0800

    FinishedCode: for loop, if statement, prints numbers except numbers with zero

--- prints_all_numbers_starting_from_0_to_100_except_numbers_ending_in_zero.py
+++ prints_all_numbers_starting_from_0_to_100_except_numbers_ending_in_zero.py
@@ -1,5 +1,7 @@
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero
 
 #for loop from 0 to 100
-#if statement to exclude numbers ending in zero
-#prints all the numbers except numbers with zero
+for i in range(0, 101):
+    if i % 10 != 0: #checks numbers not ending with zero
+        print (i)
+    
