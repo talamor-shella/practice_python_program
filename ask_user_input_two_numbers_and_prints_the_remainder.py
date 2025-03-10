dcbe76eebb57261165abfa0b3b30730f00a42b32
talamor-shella <shellatalamor@gmail.com>
2025-03-10 18:46:21 +0800

    AddedPseudocode

--- ask_user_input_two_numbers_and_prints_the_remainder.py
+++ ask_user_input_two_numbers_and_prints_the_remainder.py
@@ -1,1 +1,5 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number
+
+#ask user input two numbers
+#num1 % num2
+#print the remainder
