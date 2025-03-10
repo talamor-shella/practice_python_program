482f4625b4b3bc123a0028a92e5c5daf07aceb8f
talamor-shella <shellatalamor@gmail.com>
2025-03-10 19:44:22 +0800

    if statement to check if number is even then will add one to even count variable

--- input_ten_numbers_and_prints_how_many_are_even_numbers.py
+++ input_ten_numbers_and_prints_how_many_are_even_numbers.py
@@ -6,5 +6,9 @@ even_count = 0
 #for loop to make user input ten numbers
 for i in range(10):
     numbers = int(input(f"Enter a number {i+1}: "))
-#if statement to check if the number is even 
+
+    #if statement to check if the number is even 
+    if i % 2 == 0:
+        even_count += 1
 #even count += 1 
+print (even_count)
