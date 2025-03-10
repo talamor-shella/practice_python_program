9200e235e4f2e74bab9da984a99d9b89fa55a2a5
talamor-shella <shellatalamor@gmail.com>
2025-03-10 19:36:49 +0800

    AddedPseudocode

--- input_ten_numbers_and_prints_how_many_are_even_numbers.py
+++ input_ten_numbers_and_prints_how_many_are_even_numbers.py
@@ -1,1 +1,6 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers
+
+#set the even count to zero
+#for loop to make user input ten numbers
+#if statement to check if the number is even 
+#even count += 1 
