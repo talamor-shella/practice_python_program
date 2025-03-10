4437f5db9428a5c262aff6a9ad47145de2f56f50
talamor-shella <shellatalamor@gmail.com>
2025-03-10 17:54:23 +0800

    AskUserInputTwoNumbers

--- ask_user_input_two_numbers_and_prints_the_smaller_number.py
+++ ask_user_input_two_numbers_and_prints_the_smaller_number.py
@@ -1,5 +1,7 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number
 
 #ask user input two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
 #use min() function to return the smallest value
 #prints the smallest number
