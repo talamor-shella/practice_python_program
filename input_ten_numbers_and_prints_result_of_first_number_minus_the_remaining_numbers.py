497a946804be8b649f354c68c3ca2272bbf106c3
talamor-shella <shellatalamor@gmail.com>
2025-03-10 19:01:04 +0800

    AddedPseudocode

--- input_ten_numbers_and_prints_result_of_first_number_minus_the_remaining_numbers.py
+++ input_ten_numbers_and_prints_result_of_first_number_minus_the_remaining_numbers.py
@@ -1,1 +1,8 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers
+
+#empty list 
+#for loop to make user input numbers ten times then append it to empty list
+#assign the first number to first index
+#get the sum of remaining numbers
+#first number - sum of remaining numbers
+
