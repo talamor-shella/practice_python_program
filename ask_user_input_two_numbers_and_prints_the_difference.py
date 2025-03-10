103af54b7362484833b2a9c519b338a54bec3997
talamor-shella <shellatalamor@gmail.com>
2025-03-10 18:16:41 +0800

    AddedPseudocode

--- ask_user_input_two_numbers_and_prints_the_difference.py
+++ ask_user_input_two_numbers_and_prints_the_difference.py
@@ -1,1 +1,5 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers
+
+#ask user input two numbers
+#variable that returns the difference of two numbers
+#prints the difference
