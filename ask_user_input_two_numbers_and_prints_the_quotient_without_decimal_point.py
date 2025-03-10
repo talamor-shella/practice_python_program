718695924a4a7cb4b45c97f6401d74bb1fab5839
talamor-shella <shellatalamor@gmail.com>
2025-03-10 18:28:33 +0800

    AddedPseudocode

--- ask_user_input_two_numbers_and_prints_the_quotient_without_decimal_point.py
+++ ask_user_input_two_numbers_and_prints_the_quotient_without_decimal_point.py
@@ -1,1 +1,5 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+#ask user input two numbers
+#use floor division operator to return the nearest whole number
+#prints the quotient without decimal point
