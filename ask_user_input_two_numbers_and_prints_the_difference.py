692918df6667bf092af0deea4f3e205bb9901633
talamor-shella <shellatalamor@gmail.com>
2025-03-10 18:18:34 +0800

    AskUserInput

--- ask_user_input_two_numbers_and_prints_the_difference.py
+++ ask_user_input_two_numbers_and_prints_the_difference.py
@@ -1,5 +1,8 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers
 
 #ask user input two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 #variable that returns the difference of two numbers
 #prints the difference
