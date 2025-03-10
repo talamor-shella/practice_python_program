21e007d7c897bb7b16eb54573577cd859bb0dfbe
talamor-shella <shellatalamor@gmail.com>
2025-03-10 18:40:01 +0800

    FinishedCode: added try-except statement for zero division error

--- ask_user_input_two_numbers_and_prints_the_quotient_without_decimal_point.py
+++ ask_user_input_two_numbers_and_prints_the_quotient_without_decimal_point.py
@@ -1,11 +1,13 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+#Try and Except statement for zero division error 
+try: 
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
 
-#ask user input two numbers
-num1 = int(input("Enter a number: "))
-num2 = int(input("Enter a number: "))
+    #num1 // num2
+    quotient = num1 // num2
 
-#num1 // num2
-quotient = num1 // num2
-
-#prints the quotient without decimal point
-print(f"The quotient without decimal point is: {quotient}")
+    #prints the quotient without decimal point
+    print(f"The quotient without decimal point is: {quotient}")
+except ZeroDivisionError:
+    print("Can't Divide a number by 0")
