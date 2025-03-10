ab407ab3414ff503eddcd954d814accff05f6bdc
talamor-shella <shellatalamor@gmail.com>
2025-03-10 19:38:36 +0800

    set the even count to zero

--- input_ten_numbers_and_prints_how_many_are_even_numbers.py
+++ input_ten_numbers_and_prints_how_many_are_even_numbers.py
@@ -1,6 +1,8 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers
 
 #set the even count to zero
+even_count = 0
+
 #for loop to make user input ten numbers
 #if statement to check if the number is even 
 #even count += 1 
