acdda33d5fc6a182c7c1852c14b740bb7f06776f
talamor-shella <shellatalamor@gmail.com>
2025-03-10 18:30:20 +0800

    AskUserInputTwoNumbers

--- ask_user_input_two_numbers_and_prints_the_quotient_without_decimal_point.py
+++ ask_user_input_two_numbers_and_prints_the_quotient_without_decimal_point.py
@@ -1,5 +1,8 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
 #ask user input two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
 #use floor division operator to return the nearest whole number
 #prints the quotient without decimal point
