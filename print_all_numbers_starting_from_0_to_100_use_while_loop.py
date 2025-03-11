cad8a597baa523ccc401bbc8f86151d5477f6b17
talamor-shella <shellatalamor@gmail.com>
2025-03-11 14:55:15 +0800

    set odd number variable to 0

--- print_all_numbers_starting_from_0_to_100_use_while_loop.py
+++ print_all_numbers_starting_from_0_to_100_use_while_loop.py
@@ -1,6 +1,8 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
 #set odd number variable to 0
+odd_numbers = 0
+
 #use while loop 
 #use addition assignment operator
 #prints all the odd number
