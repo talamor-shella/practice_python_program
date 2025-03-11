1bf161ea585a407f633bdd0a025ac5ce576d34a3
talamor-shella <shellatalamor@gmail.com>
2025-03-11 15:56:26 +0800

    Added Pseudocode

--- ask_user_input_two_numbers_and_prints_all_numbers_between_the_two_numbers.py
+++ ask_user_input_two_numbers_and_prints_all_numbers_between_the_two_numbers.py
@@ -1,1 +1,6 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers
+
+#ask user input two numbers
+#if statement for swapping numbers 
+#for loop in ascending order
+#print the numbers between the two numbers
