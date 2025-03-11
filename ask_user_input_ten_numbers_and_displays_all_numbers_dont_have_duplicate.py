a2afbb72afe2601d69da39d7537f2f2b05e58e8e
talamor-shella <shellatalamor@gmail.com>
2025-03-11 21:30:58 +0800

    created an empty list

--- ask_user_input_ten_numbers_and_displays_all_numbers_dont_have_duplicate.py
+++ ask_user_input_ten_numbers_and_displays_all_numbers_dont_have_duplicate.py
@@ -1,6 +1,8 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate
 
 #make an empty list
+numbers = []
+
 #for loop that makes a user input ten numbers 
 #append it to empty list 
 #for loop and if statement 
