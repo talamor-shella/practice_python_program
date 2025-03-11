69c4abe046f825218e3d3475ec86a6fe11ffe74e
talamor-shella <shellatalamor@gmail.com>
2025-03-11 15:05:17 +0800

    FinishedCode: debugged errors: set odd_number= 1, corrects the pseudocode

--- print_all_numbers_starting_from_0_to_100_use_while_loop.py
+++ print_all_numbers_starting_from_0_to_100_use_while_loop.py
@@ -1,12 +1,15 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
-#set odd number variable to 0
-odd_numbers = 0
+#set odd number variable to 1
+odd_numbers = 1
 
 #odd_number < 101
-while odd_numbers < 101:
-    
-    #use addition assignment operator
-    odd_numbers += 2 
+while odd_numbers < 100:
+
     #prints all the odd number
     print(odd_numbers)
+
+    #use addition assignment operator
+    odd_numbers += 2
+
+    
