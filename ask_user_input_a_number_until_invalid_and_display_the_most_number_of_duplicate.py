f7b5d5885194cdc8265b7f21f330ae805305d1d5
talamor-shella <shellatalamor@gmail.com>
2025-03-12 19:31:39 +0800

    appends user input on the empty list

--- ask_user_input_a_number_until_invalid_and_display_the_most_number_of_duplicate.py
+++ ask_user_input_a_number_until_invalid_and_display_the_most_number_of_duplicate.py
@@ -1,11 +1,20 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 #empty list 
+number = []
+
 #try-except and while loop to input number until invalid
 try: 
     while True:
         num = int(input("Enter a number: "))
+
+        #append the numbers in empty list
+        number.append(num)
 except ValueError:
     print("Invalid!")
+
+print(number) #for checking only if the input numbers are on the list
+
 #for loop 
+
 # create list for duplicate
