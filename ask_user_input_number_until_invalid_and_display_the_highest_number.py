1a4f8791ccaadd70be53e7435ef762373d4d1730
talamor-shella <shellatalamor@gmail.com>
2025-03-12 22:16:10 +0800

    try-except and while loop for user input

--- ask_user_input_number_until_invalid_and_display_the_highest_number.py
+++ ask_user_input_number_until_invalid_and_display_the_highest_number.py
@@ -1,6 +1,11 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
 #try-except and while loop to keep user input number until invalid
+try:
+    while True:
+        num = int(input("Enter a number: "))
+except ValueError: 
+    print("Invalid!")
 #create an empty list
 #append user input in empty list
 #variable that returns the highest number
