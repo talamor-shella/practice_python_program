a97a885c91da1a21144327248cf6239bb466923c
talamor-shella <shellatalamor@gmail.com>
2025-03-12 14:28:51 +0800

    AddedPseudocode

--- ask_user_input_a_number_until_invalid_and_display_the_lowest_number.py
+++ ask_user_input_a_number_until_invalid_and_display_the_lowest_number.py
@@ -1,1 +1,8 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+#make an empty list 
+#try-except and while loop to continue asking until invalid
+#append the users input in empty list
+#variable that returns the lowest value
+#prints the lowest value
+
