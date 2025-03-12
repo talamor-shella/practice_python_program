2c4116b08c6bd3d83ad34aea499f2e86df345322
talamor-shella <shellatalamor@gmail.com>
2025-03-12 22:45:19 +0800

    AddedPseudocode

--- ask_user_input_a_number_until_invalid_and_display_number_from_highest_to_lowest.py
+++ ask_user_input_a_number_until_invalid_and_display_number_from_highest_to_lowest.py
@@ -1,1 +1,6 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+#try-except and while loop to continue asking until unvalid
+#empty number list
+#append user input to the list
+#sort the list from highest to lowest
