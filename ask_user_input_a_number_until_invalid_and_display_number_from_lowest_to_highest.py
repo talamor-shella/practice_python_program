6e0cdbb8c10e84536aebb6f8240f3ec92dab9384
talamor-shella <shellatalamor@gmail.com>
2025-03-12 15:29:12 +0800

    append users input to empty list and print for checking

--- ask_user_input_a_number_until_invalid_and_display_number_from_lowest_to_highest.py
+++ ask_user_input_a_number_until_invalid_and_display_number_from_lowest_to_highest.py
@@ -1,13 +1,16 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 
 #empty list
+numbers = []
 
 #try-except and while loop to make user input number until invalid 
 try:
     while True:
         num = int(input("Enter a number: "))
+        numbers.append(num)
 except ValueError:
     print("Invalid")
 
+print(numbers)
 #append the inputted numbers in empty list
 #use sort
