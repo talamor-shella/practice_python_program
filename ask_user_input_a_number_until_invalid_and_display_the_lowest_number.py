27c578a9323cfbbd8d899a832f6451662606169d
talamor-shella <shellatalamor@gmail.com>
2025-03-12 14:35:30 +0800

    append user input in empty list and print for checking the list

--- ask_user_input_a_number_until_invalid_and_display_the_lowest_number.py
+++ ask_user_input_a_number_until_invalid_and_display_the_lowest_number.py
@@ -1,14 +1,20 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 #make an empty list 
+numbers = []
 
 #try-except and while loop to continue asking until invalid
 try: 
     while True:
         num = int(input("Enter a number: "))
+        
+        #append the users input in empty list
+        numbers.append(num)
 except ValueError:
     print("Invalid")
-#append the users input in empty list
+
+print(numbers) #for checking the list of numbers
+
 #variable that returns the lowest value
 #prints the lowest value
 
