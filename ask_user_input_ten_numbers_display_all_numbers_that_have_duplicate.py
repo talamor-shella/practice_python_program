b64d68fe50cf274a67f90997be9c58a58b446de0
talamor-shella <shellatalamor@gmail.com>
2025-03-12 17:24:38 +0800

    stored the input numbers in empty list

--- ask_user_input_ten_numbers_display_all_numbers_that_have_duplicate.py
+++ ask_user_input_ten_numbers_display_all_numbers_that_have_duplicate.py
@@ -1,7 +1,14 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate
 
 #empty list
+numbers = []
+
 #for loop to make user input ten numbers
 for i in range(10):
     num = int(input(f"Enter a number {i+1}: "))
+    
+    #adds the input numbers to the list
+    numbers.append(num)
 #if statement 
+
+print(numbers)
