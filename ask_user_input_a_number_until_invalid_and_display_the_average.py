c23770c1ad70a3aa61b5372e6cc18aff1af79b52
talamor-shella <shellatalamor@gmail.com>
2025-03-12 23:03:04 +0800

    append user input in empty list

--- ask_user_input_a_number_until_invalid_and_display_the_average.py
+++ ask_user_input_a_number_until_invalid_and_display_the_average.py
@@ -1,12 +1,18 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
+#empty list
+numbers =  []
+
 #try-except and while loop
 try:
     while True:
         num = int(input("Enter a number: "))
+
+        #append user input in list
+        numbers.append(num)
 except ValueError:
     print("Invalid")
-#empty list
-#append user input in list
+
+
 #get the sum
 # result of sum divide to len of list
