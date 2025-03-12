ac11ad735efcdc268642a7c55b27afa732bab2c9
talamor-shella <shellatalamor@gmail.com>
2025-03-12 13:53:18 +0800

    created an empty list, added to empty list every numbers inputted, print for checking the list

--- ask_user_input_a_number_until_the_user_input_is_invalid_display_unique_and_duplicate_after_input.py
+++ ask_user_input_a_number_until_the_user_input_is_invalid_display_unique_and_duplicate_after_input.py
@@ -4,10 +4,19 @@ Display "Unique" after input when the inputted number don't have duplicate.
 Display "Duplicate" after input when the inputted number have duplicate.
 
 """
+#empty list
+num_list = []
+ 
 #try and except statement
 try:
     #while loop to continue asking user input until invalid 
     while True:
         num = int(input("Enter a number: "))
+        num_list.append(num)
+        
 except ValueError:
     print("invalid")
+
+#for checking the list users input 
+print(num_list)
+
