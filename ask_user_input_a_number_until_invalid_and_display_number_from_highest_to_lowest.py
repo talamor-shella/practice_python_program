36a0100f1274b958adef7d5758cc7e3d7976d03e
talamor-shella <shellatalamor@gmail.com>
2025-03-12 22:55:52 +0800

    FinishedCode: sorted list from highest to lowest

--- ask_user_input_a_number_until_invalid_and_display_number_from_highest_to_lowest.py
+++ ask_user_input_a_number_until_invalid_and_display_number_from_highest_to_lowest.py
@@ -1,12 +1,19 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
+#empty number list
+numbers = []
+
 #try-except and while loop to continue asking until unvalid
 try:
     while True:
         num = int(input("Enter a number: "))
+
+        #append user input to the list
+        numbers.append(num)
 except ValueError:
     print("Invalid!")
-    
-#empty number list
-#append user input to the list
+
 #sort the list from highest to lowest
+numbers.sort(reverse=True)
+
+print(f"Numbers from ighest to lowest: {numbers}")
