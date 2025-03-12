978c88a389ea61ffac307a14eab35bffe17c5072
talamor-shella <shellatalamor@gmail.com>
2025-03-12 22:59:27 +0800

    AddedPseudocode

--- ask_user_input_a_number_until_invalid_and_display_the_average.py
+++ ask_user_input_a_number_until_invalid_and_display_the_average.py
@@ -1,2 +1,7 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
+#try-except and while loop
+#empty list
+#append user input in list
+#get the sum 
+#result in sum / len of list
