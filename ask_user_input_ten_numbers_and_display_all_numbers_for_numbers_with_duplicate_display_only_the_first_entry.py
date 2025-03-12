f082ec0a34c961bf0accc3632a6e1002c0c8df6a
talamor-shella <shellatalamor@gmail.com>
2025-03-12 12:15:13 +0800

    AddedPseudocode

--- ask_user_input_ten_numbers_and_display_all_numbers_for_numbers_with_duplicate_display_only_the_first_entry.py
+++ ask_user_input_ten_numbers_and_display_all_numbers_for_numbers_with_duplicate_display_only_the_first_entry.py
@@ -1,1 +1,6 @@
 #Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+#empty list 
+#for loop to make user input ten numbers
+#append the ten input numbers to empty list 
+#for loop statement to display all numbers and first entry of with duplicate
