a6a80a637082bda1d2842e35dc507acd32fc0b85
talamor-shella <shellatalamor@gmail.com>
2025-03-12 12:18:22 +0800

    ask user input ten numbers

--- ask_user_input_ten_numbers_and_display_all_numbers_for_numbers_with_duplicate_display_only_the_first_entry.py
+++ ask_user_input_ten_numbers_and_display_all_numbers_for_numbers_with_duplicate_display_only_the_first_entry.py
@@ -3,6 +3,11 @@
 #empty list 
 numbers = []
 
-#for loop to make user input ten numbers
-#append the ten input numbers to empty list 
+#make user input ten numbers
+for i in range(10):
+    num = int(input(f"Enter a number {i+1}: "))
+    
+    #append the ten input numbers to empty list 
+    
+
 #for loop statement to display all numbers and first entry of with duplicate
