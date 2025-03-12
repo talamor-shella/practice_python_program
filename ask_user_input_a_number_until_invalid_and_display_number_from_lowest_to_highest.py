44d9c1ee29345da7306e144adb21d0843b6b567d
talamor-shella <shellatalamor@gmail.com>
2025-03-12 15:27:24 +0800

    try-except and while to make usuer input number until invalid

--- ask_user_input_a_number_until_invalid_and_display_number_from_lowest_to_highest.py
+++ ask_user_input_a_number_until_invalid_and_display_number_from_lowest_to_highest.py
@@ -1,6 +1,13 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 
 #empty list
+
 #try-except and while loop to make user input number until invalid 
+try:
+    while True:
+        num = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid")
+
 #append the inputted numbers in empty list
 #use sort
