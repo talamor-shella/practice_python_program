39bf329377ec87f075f9cc6a7fd8b08f1694caae
talamor-shella <shellatalamor@gmail.com>
2025-03-12 14:31:19 +0800

    try-except and while loop to continue ask user input until invalid

--- ask_user_input_a_number_until_invalid_and_display_the_lowest_number.py
+++ ask_user_input_a_number_until_invalid_and_display_the_lowest_number.py
@@ -1,7 +1,13 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 #make an empty list 
+
 #try-except and while loop to continue asking until invalid
+try: 
+    while True:
+        num = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid")
 #append the users input in empty list
 #variable that returns the lowest value
 #prints the lowest value
