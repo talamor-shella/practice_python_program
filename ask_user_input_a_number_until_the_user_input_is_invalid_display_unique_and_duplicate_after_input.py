1f1592ab0f0a284a1f3b57dd553cf88c6459b20a
talamor-shella <shellatalamor@gmail.com>
2025-03-12 14:08:23 +0800

    FinishedCode: adding if else statement inside while loop to check uniqueness or duplicate.

--- ask_user_input_a_number_until_the_user_input_is_invalid_display_unique_and_duplicate_after_input.py
+++ ask_user_input_a_number_until_the_user_input_is_invalid_display_unique_and_duplicate_after_input.py
@@ -5,18 +5,27 @@ Display "Duplicate" after input when the inputted number have duplicate.
 
 """
 #empty list
-num_list = []
- 
+numbers = []
+
 #try and except statement
 try:
     #while loop to continue asking user input until invalid 
     while True:
         num = int(input("Enter a number: "))
-        num_list.append(num)
         
-except ValueError:
-    print("invalid")
+        #checks if the numbers is not in the list 
+        if num not in numbers: 
+            
+            #prints unique if not in the list
+            print("Unique")
 
-#for checking the list users input 
-print(num_list)
+            #adds the input numbers to list
+            numbers.append(num)
+        
+        #prints duplicate if number is already in the list
+        else: 
+            print("Duplicate")
+except ValueError:
+    print("Invalid")
+
 
