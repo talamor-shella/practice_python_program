850a44aa860ed39f7b6ad15fdcc3ce5d3397e53b
talamor-shella <shellatalamor@gmail.com>
2025-03-12 22:10:59 +0800

    AddedPseudocode

--- ask_user_input_number_until_invalid_and_display_the_highest_number.py
+++ ask_user_input_number_until_invalid_and_display_the_highest_number.py
@@ -1,1 +1,7 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+#try-except and while loop to keep user input number until invalid
+#create an empty list
+#append user input in empty list
+#variable that returns the highest number
+#print the highest number in the list
