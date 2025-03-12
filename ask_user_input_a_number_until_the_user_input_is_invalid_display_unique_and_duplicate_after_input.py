263997a842617a212bbeff2b5bce88051c0df841
talamor-shella <shellatalamor@gmail.com>
2025-03-12 13:46:26 +0800

    try and except and while loop to continue asking user input until invalid

--- ask_user_input_a_number_until_the_user_input_is_invalid_display_unique_and_duplicate_after_input.py
+++ ask_user_input_a_number_until_the_user_input_is_invalid_display_unique_and_duplicate_after_input.py
@@ -4,4 +4,10 @@ Display "Unique" after input when the inputted number don't have duplicate.
 Display "Duplicate" after input when the inputted number have duplicate.
 
 """
-
+#try and except statement
+try:
+    #while loop to continue asking user input until invalid 
+    while True:
+        num = int(input("Enter a number: "))
+except ValueError:
+    print("invalid")
