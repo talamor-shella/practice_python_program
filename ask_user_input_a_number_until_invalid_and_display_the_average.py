b13aa3545f4d5c9f2e3d26e71a6506e96ce7c4be
talamor-shella <shellatalamor@gmail.com>
2025-03-12 23:01:26 +0800

    try-except and while loop for user input until invalid

--- ask_user_input_a_number_until_invalid_and_display_the_average.py
+++ ask_user_input_a_number_until_invalid_and_display_the_average.py
@@ -1,7 +1,12 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
 #try-except and while loop
+try:
+    while True:
+        num = int(input("Enter a number: "))
+except ValueError:
+    print("Invalid")
 #empty list
 #append user input in list
-#get the sum 
-#result in sum / len of list
+#get the sum
+# result of sum divide to len of list
