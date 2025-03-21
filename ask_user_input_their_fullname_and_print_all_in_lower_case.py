e15328e94676777f738d6ac3ca175f9eccd1c364
talamor-shella <shellatalamor@gmail.com>
2025-03-22 01:58:35 +0800

    ask user input and use lower() method

--- ask_user_input_their_fullname_and_print_all_in_lower_case.py
+++ ask_user_input_their_fullname_and_print_all_in_lower_case.py
@@ -1,4 +1,6 @@
 #Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
 
 #ask user input and use lower() method
+full_name = input("Enter your full name: ").lower()
+
 #print the input
