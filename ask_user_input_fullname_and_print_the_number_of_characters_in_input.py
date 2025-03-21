472e1d5adc5a91c3ac4a5d0e7525d2129d7022af
talamor-shella <shellatalamor@gmail.com>
2025-03-22 02:33:18 +0800

    added strip() in input and use len() method

--- ask_user_input_fullname_and_print_the_number_of_characters_in_input.py
+++ ask_user_input_fullname_and_print_the_number_of_characters_in_input.py
@@ -1,8 +1,9 @@
 #Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 #ask user input
-full_name = input("Enter your fullname: ")
+full_name = input("Enter your fullname: ").strip()
 
-#use count method
+#use len method
+char_count = len(full_name)
 
 #print the user input
