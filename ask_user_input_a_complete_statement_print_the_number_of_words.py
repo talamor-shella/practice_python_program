c2d1f57f9a8a664dcbbba77299df47c3034f8542
talamor-shella <shellatalamor@gmail.com>
2025-03-22 02:21:12 +0800

    AddedPseudocode

--- ask_user_input_a_complete_statement_print_the_number_of_words.py
+++ ask_user_input_a_complete_statement_print_the_number_of_words.py
@@ -1,1 +1,4 @@
 #Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+#ask user input and use split() method
+#print the user input and use len()
