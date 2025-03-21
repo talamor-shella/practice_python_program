ce8816a49d5272d8888bf7f4bdab2f6aa6a54cdf
talamor-shella <shellatalamor@gmail.com>
2025-03-22 02:01:45 +0800

    AddedPseudocode

--- ask_user_input_their_fullname_in_incorrect_casing_print_the_proper_casing.py
+++ ask_user_input_their_fullname_in_incorrect_casing_print_the_proper_casing.py
@@ -1,1 +1,4 @@
 #Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+#ask user input and use capitalize() method
+#print thhe user input
