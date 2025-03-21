476c7acaff62c0de006ae3aa2cd758eb64192f30
talamor-shella <shellatalamor@gmail.com>
2025-03-22 01:54:43 +0800

    FinishedCode: prints full name in capital letters

--- ask_user_input_their_fullname_and_print_all_in_capital_letter.py
+++ ask_user_input_their_fullname_and_print_all_in_capital_letter.py
@@ -1,7 +1,7 @@
 #Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
-#ask user input their full name
-full_name = input("Enter your fullname").upper()
+#ask user input their full name and use upper method
+full_name = input("Enter your fullname: ").upper()
 
-#use upper() method
 #prints the full name in all capital letter
+print(full_name)
