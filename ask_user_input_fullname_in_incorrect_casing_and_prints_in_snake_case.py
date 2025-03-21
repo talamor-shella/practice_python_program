6932db81674dd59849b7ccf5e2fa0b4b08d159b0
talamor-shella <shellatalamor@gmail.com>
2025-03-22 02:51:05 +0800

    ask user input and use lower() method

--- ask_user_input_fullname_in_incorrect_casing_and_prints_in_snake_case.py
+++ ask_user_input_fullname_in_incorrect_casing_and_prints_in_snake_case.py
@@ -1,5 +1,7 @@
 #Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
 #ask user input and use lower() method
+full_name = input("Enter your fullname: ").lower()
+
 #use replace() method
 #print input in snake case
