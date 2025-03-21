4f01a560ff9748eaf8ed8d3e2700784433da0759
talamor-shella <shellatalamor@gmail.com>
2025-03-22 02:06:32 +0800

    FinisedCode: fixed error used title method and prints the input in proper casing

--- ask_user_input_their_fullname_in_incorrect_casing_print_the_proper_casing.py
+++ ask_user_input_their_fullname_in_incorrect_casing_print_the_proper_casing.py
@@ -1,6 +1,7 @@
 #Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
-#ask user input and use capitalize() method
-full_name = input("Enter your fullname: ").capitalize()
+#ask user input and use title() method
+full_name = input("Enter your fullname: ").title()
 
 #print thhe user input
+print(full_name)
