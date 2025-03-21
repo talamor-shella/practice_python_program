50fef98a110eeee745fe8b3167f41da398577294
talamor-shella <shellatalamor@gmail.com>
2025-03-22 01:48:35 +0800

    use zfill() method

--- ask_user_input_a_number_from_0_to_1000_print_the_number_in_6digit_format.py
+++ ask_user_input_a_number_from_0_to_1000_print_the_number_in_6digit_format.py
@@ -1,7 +1,9 @@
 #Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 #ask user input a number 
-number = int(input("Enter a number: "))
+number = (input("Enter a number: "))
 
 #use zfill method
+fill_zeros = number.zfill(6)
+
 #print the number in 6 digit format
