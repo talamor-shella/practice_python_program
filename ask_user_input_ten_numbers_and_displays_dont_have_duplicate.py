3e91910171a01172c598f728f60f779ccd5c9cd1
talamor-shella <shellatalamor@gmail.com>
2025-03-22 03:07:32 +0800

    FinishedCode- displays numbers dont have duplicate

---
+++ ask_user_input_ten_numbers_and_displays_dont_have_duplicate.py
@@ -0,0 +1,23 @@
+#Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate
+
+#make an empty list
+numbers = []
+
+#for loop that makes a user input ten numbers 
+for i in range(10):
+    num = int(input(f"Enter a number {i+1}: "))
+    
+    #append it to empty list 
+    numbers.append(num)
+
+#gives the user lists of input numbers
+print(f"list of numbers: {numbers}")
+
+#for loop and if statement 
+for num in numbers: 
+    if numbers.count(num) == 1: #counts the numbers and checks the uniqueness of numbers
+        
+        #prints the numbers that dont have duplicate
+    print(num)
+
+    
