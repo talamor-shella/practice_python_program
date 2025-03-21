67cd92313173184682ee2ff25e0d51b92651251b
talamor-shella <shellatalamor@gmail.com>
2025-03-22 02:03:08 +0800

    ask user input and use capitlize() method

--- ask_user_input_their_fullname_in_incorrect_casing_print_the_proper_casing.py
+++ ask_user_input_their_fullname_in_incorrect_casing_print_the_proper_casing.py
@@ -1,4 +1,6 @@
 #Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
 #ask user input and use capitalize() method
+full_name = input("Enter your fullname: ").capitalize()
+
 #print thhe user input
