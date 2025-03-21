db8ce90babfe073e09e3f3044e90230cbcadc67c
talamor-shella <shellatalamor@gmail.com>
2025-03-22 02:22:11 +0800

    ask user input and split() method

--- ask_user_input_a_complete_statement_print_the_number_of_words.py
+++ ask_user_input_a_complete_statement_print_the_number_of_words.py
@@ -1,4 +1,6 @@
 #Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
 #ask user input and use split() method
+full_name = input("Enter a statement: ").split()
+
 #print the user input and use len()
