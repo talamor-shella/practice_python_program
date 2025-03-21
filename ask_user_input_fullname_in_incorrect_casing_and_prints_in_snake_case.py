a7af00891e5a7303fe31a82bc2590dee8c3f7521
talamor-shella <shellatalamor@gmail.com>
2025-03-22 02:49:42 +0800

    AddedPseudocode

--- ask_user_input_fullname_in_incorrect_casing_and_prints_in_snake_case.py
+++ ask_user_input_fullname_in_incorrect_casing_and_prints_in_snake_case.py
@@ -1,1 +1,5 @@
 #Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+#ask user input and use lower() method
+#use replace() method
+#print input in snake case
